10371442e399f909d73ba4d6f001c10fe3f27e3d
benoitx <benoitx@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2007-01-12 18:49:55 +0000

    a couple of changes.
    git-svn-id: https://statsvn.svn.sourceforge.net/svnroot/statsvn/trunk@263 70feae74-860a-0410-b6e4-f24f7a5e8c4f

--- statsvn.bat
+++ statsvn.bat
@@ -1,3 +1,3 @@
 svn log -v --xml https://svn.sourceforge.net/svnroot/statsvn/ > svn.log
 rem mkdir qalab\target\docs\statsvn
-java -jar statsvn\dist\statsvn.jar -xdoc -output-dir statsvn\site\statsvn -tags "^0.2.0|^0.1.3|^0.1.2|^0.0.9" -title StatSVN -exclude "**/qalab.xml|**/*.pdf|**/etc/LICENSE" -viewvc http://svn.sourceforge.net/viewvc/statsvn/trunk ./svn.log .
+java -jar statsvn\dist\statsvn.jar -verbose -xdoc -output-dir statsvn\site\statsvn -tags "^0.2.0|^0.1.3|^0.1.2|^0.0.9" -title StatSVN -exclude "**/qalab.xml|**/*.pdf|**/etc/LICENSE" -viewvc http://svn.sourceforge.net/viewvc/statsvn/trunk ./svn.log .

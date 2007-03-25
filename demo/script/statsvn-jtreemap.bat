c566d96e7e055f923dd3a51b346e6dbe6be804d9
benoitx <benoitx@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2007-03-25 23:40:04 +0000

    Added details for users.
    git-svn-id: https://statsvn.svn.sourceforge.net/svnroot/statsvn/trunk@316 70feae74-860a-0410-b6e4-f24f7a5e8c4f

--- demo/script/statsvn-jtreemap.bat
+++ demo/script/statsvn-jtreemap.bat
@@ -1,5 +1,5 @@
 cd \statsvn\demos\jtreemap
 svn update
 svn log -v --xml https://jtreemap.svn.sourceforge.net/svnroot/jtreemap/ > svn.log
-java -mx512m -jar \statsvn\lib\statsvn.jar -threads 5 -xdoc -concurrency-threshold 2000 -tags "^1.0.1|^1.1.0"  -output-dir /statsvn/site/src/xdocs/jtreemap -title JTreeMap -exclude "**/qalab.xml" -viewvc http://jtreemap.svn.sourceforge.net/viewvc/jtreemap/trunk ./svn.log .
+java -mx512m -jar \statsvn\lib\statsvn.jar -threads 5 -xdoc -config-file ./statsvn.properties -concurrency-threshold 2000 -tags "^1.0.1|^1.1.0"  -output-dir /statsvn/site/src/xdocs/jtreemap -title JTreeMap -exclude "**/qalab.xml" -viewvc http://jtreemap.svn.sourceforge.net/viewvc/jtreemap/trunk ./svn.log .
 svn cleanup
--- demo/script/statsvn-testng.bat
+++ demo/script/statsvn-testng.bat
@@ -1,5 +1,5 @@
 cd \statsvn\demos\testng
 svn update
 svn log -v --xml http://testng.googlecode.com/svn > svn.log
-java -mx512m -jar \statsvn\lib\statsvn.jar -threads 18 -xdoc -concurrency-threshold 2000 -exclude "**/*.htaccess" -verbose -output-dir /statsvn/site/src/xdocs/testng -title TestNG -viewvc http://testng.googlecode.com/svn/trunk/ ./svn.log .
+java -mx512m -jar \statsvn\lib\statsvn.jar -threads 18 -xdoc -config-file ./testng.properties -concurrency-threshold 2000 -exclude "**/*.htaccess" -verbose -output-dir /statsvn/site/src/xdocs/testng -title TestNG -viewvc http://testng.googlecode.com/svn/trunk/ ./svn.log .
 svn cleanup

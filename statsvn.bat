278cc3cec267f68506b3d9deb3fa64ee44484950
benoitx <benoitx@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2007-04-09 11:02:20 +0000

    using the demo config file for StatSVN.
    git-svn-id: https://statsvn.svn.sourceforge.net/svnroot/statsvn/trunk@328 70feae74-860a-0410-b6e4-f24f7a5e8c4f

--- statsvn.bat
+++ statsvn.bat
@@ -1,3 +1,3 @@
 svn log -v --xml https://statsvn.svn.sourceforge.net/svnroot/statsvn/ > svn.log
 rem mkdir qalab\target\docs\statsvn
-java -jar statsvn\dist\statsvn.jar -verbose -xdoc -config-file ./statsvn.properties -output-dir statsvn\site\statsvn -tags "^0.2.0|^0.1.3|^0.1.2|^0.0.9|^0.3.0|^0.3.1|^0.3.2" -title StatSVN -exclude "**/qalab.xml|**/*.pdf|**/etc/LICENSE" -viewvc http://svn.sourceforge.net/viewvc/statsvn/trunk ./svn.log .
+java -jar statsvn\dist\statsvn.jar -verbose -xdoc -config-file ./demo/statsvn.properties -output-dir statsvn\site\statsvn -tags "^0.2.0|^0.1.3|^0.1.2|^0.0.9|^0.3.0|^0.3.1|^0.3.2" -title StatSVN -exclude "**/qalab.xml|**/*.pdf|**/etc/LICENSE" -viewvc http://svn.sourceforge.net/viewvc/statsvn/trunk ./svn.log .

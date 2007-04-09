c55f8822461f6ea84d80357dc6974b96f3589a5f
benoitx <benoitx@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2007-04-09 23:15:03 +0000

    Use variables.
    git-svn-id: https://statsvn.svn.sourceforge.net/svnroot/statsvn/trunk@335 70feae74-860a-0410-b6e4-f24f7a5e8c4f

--- demo/script/statsvn-groovy.bat
+++ demo/script/statsvn-groovy.bat
@@ -1,6 +1,6 @@
 title Groovy
-cd \statsvn\demos\groovy
+cd %STATSVN_HOME%\demos\groovy
 svn update
 svn log -v --xml http://svn.codehaus.org/groovy/ > svn.log
-java -mx712m -jar \statsvn\lib\statsvn.jar -threads 15 -xdoc -concurrency-threshold 2000 -tags "^GROOVY_1_0" -verbose -output-dir /statsvn/site/src/xdocs/groovy -title Groovy -viewvc http://svn.groovy.codehaus.org/browse/groovy/trunk ./svn.log .
+java -mx712m -jar %STATSVN_HOME%\lib\statsvn.jar -threads 15 -xdoc -concurrency-threshold 2000 -tags "^GROOVY_1_0" -verbose -output-dir %STATSVN_HOME%/site/src/xdocs/groovy -title Groovy -viewvc http://svn.groovy.codehaus.org/browse/groovy/trunk ./svn.log .
 svn cleanup
--- demo/script/statsvn-spring-richclient.bat
+++ demo/script/statsvn-spring-richclient.bat
@@ -1,6 +1,6 @@
 title Spring-Rich-Client
-cd \statsvn\demos\spring-richclient
+cd %STATSVN_HOME%\demos\spring-richclient
 svn update
 svn log -v --xml https://svn.sourceforge.net/svnroot/spring-rich-c > svn.log
-java -mx512m -jar \statsvn\lib\statsvn.jar -config-file /statsvn/script/springrc.properties -tags "^spring-richclient-0.2.1|^spring-richclient-0.2.0" -output-dir /statsvn/site/src/xdocs/spring-richclient -title Spring-RC -viewvc http://svn.sourceforge.net/viewvc/spring-rich-c/trunk/spring-richclient -threads 15 -xdoc -concurrency-threshold 2000 -verbose ./svn.log .
+java -mx512m -jar %STATSVN_HOME%\lib\statsvn.jar -config-file %STATSVN_HOME%/script/springrc.properties -tags "^spring-richclient-0.2.1|^spring-richclient-0.2.0" -output-dir %STATSVN_HOME%/site/src/xdocs/spring-richclient -title Spring-RC -viewvc http://svn.sourceforge.net/viewvc/spring-rich-c/trunk/spring-richclient -threads 15 -xdoc -concurrency-threshold 2000 -verbose ./svn.log .
 svn cleanup

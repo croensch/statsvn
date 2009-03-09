57ef6def21bd5db19270866a5104264035efb791
benoitx <benoitx@70feae74-860a-0410-b6e4-f24f7a5e8c4f>
2009-03-09 22:49:55 +0000

    Getting ready for a 0.5.0
    git-svn-id: https://statsvn.svn.sourceforge.net/svnroot/statsvn/trunk@375 70feae74-860a-0410-b6e4-f24f7a5e8c4f

--- statsvn.bat
+++ statsvn.bat
@@ -1,3 +1,3 @@
 svn log -v --xml https://statsvn.svn.sourceforge.net/svnroot/statsvn/ > svn.log
 rem mkdir qalab\target\docs\statsvn
-java -jar statsvn\dist\statsvn.jar -debug -verbose -charset ISO-8859-1 -xdoc -config-file ./demo/script/statsvn.properties -output-dir statsvn\site\statsvn -tags "^0.2.0|^0.1.3|^0.1.2|^0.0.9|^0.3.0|^0.3.1|^0.3.2|^0.4.0|^0.4.1" -title StatSVN -exclude "**/qalab.xml|**/*.pdf|**/etc/LICENSE" -viewvc http://svn.sourceforge.net/viewvc/statsvn/trunk ./svn.log .
+java -jar statsvn\dist\statsvn.jar -debug -verbose -charset ISO-8859-1 -xdoc -config-file ./demo/script/statsvn.properties -output-dir statsvn\site\statsvn -tags "^0.2.0|^0.1.3|^0.1.2|^0.0.9|^0.3.0|^0.3.1|^0.3.2|^0.4.0|^0.4.1|^0.5.0" -title StatSVN -exclude "**/qalab.xml|**/*.pdf|**/etc/LICENSE" -viewvc http://svn.sourceforge.net/viewvc/statsvn/trunk ./svn.log .
